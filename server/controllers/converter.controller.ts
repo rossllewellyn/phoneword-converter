c175d4925470cc60254528cda3d3406a6a967a52
rossllewellyn <rossllewellyn95@hotmail.co.uk>
2022-06-18 13:43:50 +0100

    converter controller

--- server/controllers/converter.controller.ts
+++ server/controllers/converter.controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
 
+import { convertPhonewordToWord } from "./converter.helper";
+
 export const convertPhonewordToWordController = (
-  req: Request,
+  req: Request<{ phoneword: string }>,
   res: Response
 ): void => {
   console.log("Function convertPhonewordToWordController Execution Started");
   try {
+    const { phoneword } = req.params;
+    const text: string[] = convertPhonewordToWord(phoneword);
+    res.status(200);
+    res.send(text);
   } catch (err) {
     console.error(err);
     res.status(500);

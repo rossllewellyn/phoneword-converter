4f47022bf1321d9e5f8241e94ff1aab1471f55b3
rossllewellyn <rossllewellyn95@hotmail.co.uk>
2022-06-19 10:29:55 +0100

    cors

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import converterRoute from "./routes/converter.route";
 
 config();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3005;
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", converterRoute);
 

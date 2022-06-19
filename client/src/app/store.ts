bfb283493890f6a5053d21104259b3bf543a9a38
rossllewellyn <rossllewellyn95@hotmail.co.uk>
2022-06-19 11:58:10 +0100

    styling and removed unneeded files

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import phonewordReducer from "../features/phone/phonewordSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     phoneword: phonewordReducer,
   },
 });
